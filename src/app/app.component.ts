ca884ce656b0c768557e914b8aa90208728af3dd
Dwilsondesign25 <dwilsondesign25@gmail.com>
2024-11-05 11:14:00 -0500

    Add Environment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  console.log(environment.apiURL);
+
   title = 'drewfreestyle';
 }
